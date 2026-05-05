b4fd5ec193a36453e2c3f5acebeda049653df757
Daniel Paez <dfpaez@unimagdalena.edu.co>
2026-05-05 17:58:05 -0500

    Small changes to Class1.cs, added some using directives.

--- src/SOEA.Domain/Class1.cs
+++ src/SOEA.Domain/Class1.cs
@@ -1,4 +1,31 @@
-﻿namespace SOEA.Domain;
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace SOEA.Domain
+{
+    public enum TipoEspacio
+    {
+        Salon,
+        Laboratorio,
+        Audirorio
+    }
+    public enum DiaDeSemana
+    {
+        Lunes,
+        Martes,
+        Miercoles,
+        Jueves,
+        Viernes
+    }
+    public enum TipoAlternancia
+    {
+        Normal,
+        semanal,
+        biSemanal
+    }
+    
+}
 
 public class Class1
 {
